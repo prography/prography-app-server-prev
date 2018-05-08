85307cef0c044ef9159ed51b441aff977996725b
이승준 <rbfaosldk@hubilon.com>
2018-05-08 21:55:38 +0900

    최초 커밋

---
+++ src/main/java/com/prography/appdev3/AppDev3Application.java
@@ -0,0 +1,53 @@
+package com.prography.appdev3;
+
+import java.nio.charset.Charset;
+
+import javax.servlet.Filter;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@SpringBootApplication
+public class AppDev3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDev3Application.class, args);
+	}
+	
+
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+	}
+	
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	@Bean
+	public Filter characterEncodingFilter() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		return filter;
+	}
+
+	
+	@Bean   
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource)throws Exception{      
+	   SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();     
+	   sessionFactory.setDataSource(dataSource);    
+	   Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*Mapper.xml");           
+	   sessionFactory.setMapperLocations(res);   
+	   
+	   	return sessionFactory.getObject();   
+	}
+}
